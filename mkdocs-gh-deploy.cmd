08101addbd6e2dc2eab4d01cc13f59a9203beb22
afoolsbag <afoolsbag@gmail.com>
2020-12-18 14:10:44 +0800

    chore: 更新以适配 GitHub 发布分支

--- mkdocs-gh-deploy.cmd
+++ mkdocs-gh-deploy.cmd
@@ -14,7 +14,7 @@ CD "%script_directory%" ^
         && CALL :pause_if_double_click ^
         && EXIT /B 2
 
-mkdocs gh-deploy --remote-branch master --strict --site-dir "%site_directory%" ^
+mkdocs gh-deploy --remote-branch "www" --strict --site-dir "%site_directory%" ^
         && ECHO MkDocs gh-deploy succeed. ^
         || ECHO MkDocs gh-deploy failed. ^
         && CALL :pause_if_double_click ^
