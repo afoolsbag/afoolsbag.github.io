f94a2faa2cf41c02646bc53d5f8aff19e7659013
zhengrr <zhengrr0@gmail.com>
2021-09-26 12:10:36 +0800

    chore: 改用更新的 %CMDCMDLINE% 双击运行判定

--- scoop-update.cmd
+++ scoop-update.cmd
@@ -17,6 +17,6 @@ CALL :pause_if_double_click
 EXIT /B 0
 
 :pause_if_double_click
-        ECHO %CMDCMDLINE% | FINDSTR /L /B %COMSPEC% > NUL ^
+        ECHO %CMDCMDLINE% | FINDSTR /I /C:" /C " > NUL ^^
                 && PAUSE
         EXIT /B 0
