7496f8abc969f83109cfdf9184a04967f6910f24
zhengrr <afoolsbag@gmail.com>
2022-11-15 09:12:33 +0800

    chore: 使用 pipenv 和 invoke 替代脚本；调整第三方库的 CDN 提供商

---
+++ tasks.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""\
+:Version:   2022-11-14
+:Since:     2020-05-15
+:Author:    zhengrr
+:Copyright: UNLICENSE
+
+"""
+# https://github.com/pyinvoke/invoke/issues/833
+import inspect
+
+if not hasattr(inspect, "getargspec"):
+    inspect.getargspec = inspect.getfullargspec
+
+from invoke import task
+
+
+@task
+def env(c):
+    c.run("python -m pip install --upgrade pip")
+    c.run("pip install --upgrade pipenv")
+    c.run("pipenv install --dev")
+
+
+@task
+def update(c):
+    c.run("python -m pip install --upgrade pip")
+    c.run("pip install --upgrade pipenv")
+    c.run("pipenv update --dev")
+
+
+@task
+def build(c):
+    c.run("pipenv run mkdocs build --strict")
+
+
+@task(aliases=["gh", "deploy"])
+def gh_deploy(c):
+    c.run('pipenv run gh-deploy --remote-branch "www" --strict --site-dir "site"')
+
+
+@task(aliases=["ls"], default=True)
+def local_serve(c):
+    c.run('pipenv run mkdocs serve --dev-addr 127.0.0.1:49152')
+
+
+@task(aliases=["rs"])
+def remote_serve(c):
+    c.run('pipenv run mkdocs serve --dev-addr 0.0.0.0:80')
+
+
+@task
+def clear(c):
+    c.run("pipenv --rm")
